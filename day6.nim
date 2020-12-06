3f7b65df39f2083cfc703f3b0612a8ad889ecc92
kayhoogland <kay@floryn.com>
2020-12-06 10:23:26 +0100

    Add day 6

---
+++ day6.nim
@@ -0,0 +1,16 @@
+import strutils, sequtils, tables
+
+proc solve(path: string): (int, int) =
+  let answers = readFile path
+
+  for answer in answers.split("\n\n"):
+    var groupAnswer = replace(answer, "\n", "")
+    var letterFrequencies = toCountTable(group_answer)
+
+    result[0] += len(letterFrequencies)
+    
+    var groupSize = countLines(answer.strip())
+    var countSeq = toSeq(letterFrequencies.values)
+    result[1] += len(countSeq.filterIt(it == groupSize))
+
+echo solve "./data/day6.txt"
